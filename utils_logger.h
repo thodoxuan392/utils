c102be287e14bc0b6e442619938d92a6502700b8
Xuan Tho Do <tho.dx@novaintechs.com>
2024-02-21 23:38:38 +0700

    feat: change uartId

--- utils_logger.h
+++ utils_logger.h
@@ -13,12 +13,13 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-#define UTILS_LOG(data, len)	UART_send( UART_ID_2, data, len, 0xFFFF)
-#define UTILS_LOG_TIME_MS() 		(xTaskGetTickCount() * 1000 / configTICK_RATE_HZ)
+#define UTILS_LOG(data, len) UART_send(UART_ID_0, data, len, 0xFFFF)
+#define UTILS_LOG_TIME_MS() (xTaskGetTickCount() * 1000 / configTICK_RATE_HZ)
 #define UTILS_MAX_LOG_BUFFER 1024
 
-typedef enum{
-    UTILS_LOG_OFF,
+typedef enum
+{
+	UTILS_LOG_OFF,
 	UTILS_LOG_FATAL,
 	UTILS_LOG_ERROR,
 	UTILS_LOG_WARN,
@@ -26,19 +27,17 @@ typedef enum{
 	UTILS_LOG_DEBUG,
 	UTILS_LOG_TRACE,
 	UTILS_LOG_ALL
-}utils_log_level_t;
+} utils_log_level_t;
 
 #define utils_log_trace(...) utils_log_log(UTILS_LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define utils_log_debug(...) utils_log_log(UTILS_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define utils_log_info(...)  utils_log_log(UTILS_LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define utils_log_warn(...)  utils_log_log(UTILS_LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define utils_log_info(...) utils_log_log(UTILS_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define utils_log_warn(...) utils_log_log(UTILS_LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
 #define utils_log_error(...) utils_log_log(UTILS_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define utils_log_fatal(...) utils_log_log(UTILS_LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
-
 void utils_log_init(utils_log_level_t level);
-void utils_log_log(utils_log_level_t level, const char *file, int line, const char *fmt, ...);
-void utils_log_raw(const char *fmt, ...);
-
+void utils_log_log(utils_log_level_t level, const char* file, int line, const char* fmt, ...);
+void utils_log_raw(const char* fmt, ...);
 
 #endif /* UTILS_LOGGER_H_ */
